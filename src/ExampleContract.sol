a185802298f70200b72ee94d620008d10f61bfc6
Johnathan Bailey <96433970+jjbailey-io@users.noreply.github.com>
2025-05-01 18:01:35 -0400

    Updating format to pass CI check

--- src/ExampleContract.sol
+++ src/ExampleContract.sol
@@ -1,15 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
+
 import "./RulesEngineClientCustom.sol";
 
 /**
  * @title Rules Engine Client Custom Modifiers
  */
 contract ExampleContract is RulesEngineClientCustom {
-    function transfer(
-        address to,
-        uint256 value
-    ) public checkRulesBefore(to, value) {
+    function transfer(address to, uint256 value) public checkRulesBefore(to, value) {
         // this function is purposefully empty
     }
 }
