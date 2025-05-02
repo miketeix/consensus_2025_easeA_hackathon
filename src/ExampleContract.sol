68ac3da4f2ad8e77af45a01fe6909de71dd10271
Johnathan Bailey <96433970+jjbailey-io@users.noreply.github.com>
2025-05-02 15:39:17 -0400

    Running format for CI happiness

--- src/ExampleContract.sol
+++ src/ExampleContract.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.13;
  * An example contract that does nothing.
  * This contract is used to demonstrate the use of the `transfer` function.
  */
+
 contract ExampleContract {
     function transfer(address to, uint256 value) public returns (bool) {
         // this function is purposefully empty
